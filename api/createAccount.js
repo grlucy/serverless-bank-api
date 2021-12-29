0facbe291400b429831052756dfabf5bae1d48b4
Gina Lucy <glucy@paymerang.com>
2021-12-29 16:44:52 -0500

    Add updateBalance function

--- api/createAccount.js
+++ api/createAccount.js
@@ -2,7 +2,12 @@ const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb')
 const client = new DynamoDBClient()
 
 module.exports.handler = async (event, context) => {
-  const response = {}
+  const response = {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    }
+  }
   const newAccountID = Math.random().toString().slice(2,11) // Generate 9-digit ID with left-padded zeros
 
   const params = {
@@ -16,15 +21,11 @@ module.exports.handler = async (event, context) => {
   try {
     const data = await client.send(new PutItemCommand(params))
     response.statusCode = 200
-    response.body = JSON.stringify({ message: 'Created account', account: newAccountID, data })
+    response.body = JSON.stringify({ message: 'Created account', account: newAccountID })
   } catch (error) {
     console.error('createAccount error:', error)
     response.statusCode = 500
     response.body = JSON.stringify({ message: 'Failed to create account', error })
-  }
-  response.headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true
   }
   return response
 }
---
+++ api/updateBalance.js
@@ -0,0 +1,43 @@
+const { DynamoDBClient, UpdateItemCommand } = require('@aws-sdk/client-dynamodb')
+const client = new DynamoDBClient()
+
+module.exports.handler = async (event, context) => {
+  const response = {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+  }
+  const account = event.queryStringParameters?.account
+  const amount = event.queryStringParameters?.amount
+
+  if (!account || !amount) {
+      response.statusCode = 422
+      response.body = JSON.stringify({ error: 'Missing parameter' })
+      return response  
+  }
+
+  const params = {
+    TableName: process.env.ddbTableName,
+    Key: {
+        account: { N: account }
+    },
+    UpdateExpression: 'SET balance = if_not_exists(balance, :initial) + :num',
+    ExpressionAttributeValues: {
+        ':num': { N: amount },
+        ':initial': { N: '0' },
+    },
+    ReturnValues: 'UPDATED_NEW'
+  }
+
+  try {
+    const data = await client.send(new UpdateItemCommand(params))
+    response.statusCode = 200
+    response.body = JSON.stringify({ message: 'Updated balance', newBalance: Number(data.Attributes.balance.N) })
+  } catch (error) {
+    console.error('updateBalance error:', error)
+    response.statusCode = 500
+    response.body = JSON.stringify({ message: 'Failed to update balance', error })
+  }
+  return response
+}
